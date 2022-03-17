bc212c282d5ebcca8a273f3c459a9c49dbbe3ec0
Martin Stone <56mstone@gmail.com>
2022-03-16 23:10:47 -0300

    (feat(01): subiendo a la nube

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Buen dia Martin!')
+  res.send('Buen dia Martin!, Transmitiendo desde AWS')
 })
 
 app.listen(port, () => {
